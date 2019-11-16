3281c91381f57fdd47b2a89c348bf7b70bef96d3
Jim Watkins <jim@bigskyway.com>
2019-11-16 12:27:50 -0600

    added nginx, so ssh tinkering

---
+++ util/addssh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Connect to a host and add an ssh key to a user
+# parms: host, user, pk to add
+
+
+
+# connect to client
--- util/deploy.sh
+++ util/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+rm -rf /etc/ansible
 cp -Rv ../inv/ /etc/ansible
