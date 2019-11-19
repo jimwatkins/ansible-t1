149b7fdc247c92ed6f1b68ed2ba27fa4b15cb971
Jim Watkins <jim@bigskyway.com>
2019-11-18 23:56:13 -0600

    Add keys stubbed in

---
+++ util/addkeys.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./addsh.sh ../pubkeys/ansible.pub jim 192.168.1.154
--- util/addssh.sh
+++ util/addssh.sh
@@ -3,6 +3,13 @@
 # Connect to a host and add an ssh key to a user
 # parms: host, user, pk to add
 
+KEY=$1
+SSHUSER=$2
+SSHHOST=$3
 
+
+cat ${KEY} | ssh ${SSHUSER}@${SSHHOST} "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
+
+# cat ~/.ssh/id_rsa.pub | ssh user@192.168.1.1 "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
 
 # connect to client
